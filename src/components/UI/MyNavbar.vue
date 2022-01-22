af4c8ccba525cd7d92da096d1066540aafb54be3
Vlad Vlad <burdukovskij.vlad@mail.ru>
2022-01-22 17:08:11 +0300

    router

---
+++ src/components/UI/MyNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="navbar">
+    <my-button @click="$router.push('/')">Главная</my-button>
+    <my-button @click="$router.push('/posts')">Посты</my-button>
+    <my-button @click="$router.push('/about')">О нас</my-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+ 
+  
+}
+</script>
+<style lang="scss">
+  .navbar {
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    background: rgba(0, 0, 0, 0.3);
+  }
+</style>
